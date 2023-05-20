15c985f38436cfa05636011c8d4f4ae8ca22d0ef
Md.Mohiuddin <saddammd203@gmail.com>
2023-05-20 01:23:09 -0700

    added footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,32 @@
+const Footer = () => {
+    return (
+        <footer className="footer p-10 bg-base-200 text-base-content sticky top-[100vh] bg-gradient-to-l from-indigo-500 to-indigo-000 rounded-lg text-yellow-900 text-sm font-semibold">
+        <div>
+        <img className='rounded-lg' src="https://i.ibb.co/mGcZ88J/image.png" alt="" />
+          <p>ACME Industries Ltd.<br/>Providing reliable tech since 1992</p>
+        </div> 
+        <div>
+          <span className="footer-title font-bold">Services</span> 
+          <a className="link link-hover">Branding</a> 
+          <a className="link link-hover">Design</a> 
+          <a className="link link-hover">Marketing</a> 
+          <a className="link link-hover">Advertisement</a>
+        </div> 
+        <div>
+          <span className="footer-title">Company</span> 
+          <a className="link link-hover">About us</a> 
+          <a className="link link-hover">Contact</a> 
+          <a className="link link-hover">Jobs</a> 
+          <a className="link link-hover">Press kit</a>
+        </div> 
+        <div>
+          <span className="footer-title">Legal</span> 
+          <a className="link link-hover">Terms of use</a> 
+          <a className="link link-hover">Privacy policy</a> 
+          <a className="link link-hover">Cookie policy</a>
+        </div>
+      </footer>
+    );
+};
+
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,16 @@ export default {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'hero-pattern': "url('https://i.ibb.co/kxXP2Jb/pexels-mart-production-7334328.jpg')",
+        'footer-texture': "url('https://i.ibb.co/kxXP2Jb/pexels-mart-production-7334328.jpg')",
+      },
       fontFamily: {
         primary: "",
       },
     },
   },
+  // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
 }
 

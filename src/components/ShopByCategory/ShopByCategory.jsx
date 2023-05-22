183588d20179494d2e9b591d555d122ebd38cb95
Md.Mohiuddin <saddammd203@gmail.com>
2023-05-23 00:13:17 +0600

    create shop by category

--- src/components/ShopByCategory/ShopByCategory.jsx
+++ src/components/ShopByCategory/ShopByCategory.jsx
@@ -1,8 +1,91 @@
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 
 const ShopByCategory = () => {
     return (
         <div>
-            <h1>Shop By Category</h1>
+ <div className="text-center">
+ <h2 className="text-4xl font-extrabold mb-6">Shop By Category</h2>
+             <Tabs className="text-lg">
+    <TabList>
+      <Tab>Educational Math Toys</Tab>
+      <Tab>Educational Engineering Toys</Tab>
+      <Tab>Educational Science Toys </Tab>
+    </TabList>
+
+    <TabPanel>
+    <div className="flex justify-center gap-10 m-10">
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    </div>
+    </TabPanel>
+    <TabPanel>
+    <div className="flex justify-center gap-10 m-10">
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    </div>
+    </TabPanel>
+    <TabPanel>
+    <div className="flex justify-center gap-10 m-10">
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    </div>
+    </TabPanel>
+  </Tabs>
+ </div>
         </div>
     );
 };

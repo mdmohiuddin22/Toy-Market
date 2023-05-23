5adab7cdb332e64c4b38e500d2c950c04f301a6d
Md.Mohiuddin <saddammd203@gmail.com>
2023-05-23 22:39:42 +0600

    added shop by category

--- src/components/ShopByCategory/ShopByCategory.jsx
+++ src/components/ShopByCategory/ShopByCategory.jsx
@@ -24,46 +24,30 @@ const ShopByCategory = () => {
     <TabPanel>
     <div className="flex justify-center gap-10 m-10">
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <figure><img src="
+  https://i.ibb.co/ByMg6TK/stock-photo-wooden-toy-blocks-school-supplies-math-fractions-pencils-numbers-on-beige-background-bac.jpg
+  " alt="" /></figure>
   <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+
+    <h2 className="card-title">Number game board</h2>
+    <p className="text-lg">price:$19.99</p>
+    <h2 className="text-lg">Rating: 4.5</h2>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
+     <button className="btn btn-primary">View Details</button>
+     </div>
+  
+
   </div>
 </div>
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <figure><img src="https://i.ibb.co/HqRfFHy/stock-photo-wooden-figures-of-the-montessori-methodology-math-kids-counting-puzzle-for-kids-educatio.jpg" alt="" /></figure>
   <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 className="card-title">Cash Register</h2>
+    <p className="text-lg">price:$19.99</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
-    </div>
-    </TabPanel>
-    <TabPanel>
-    <div className="flex justify-center gap-10 m-10">
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
-    </div>
-  </div>
-</div>
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <h2 className="text-lg">Rating: 4.2</h2>
+      <br/>
+      <button  className="btn btn-primary justify-center">View Details</button>
     </div>
   </div>
 </div>
@@ -72,22 +56,50 @@ const ShopByCategory = () => {
     <TabPanel>
     <div className="flex justify-center gap-10 m-10">
     <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+  <figure><img src="https://i.ibb.co/Nmpt8Hq/stock-photo-little-boy-years-old-playing-chemist-chemistry-game-at-home-pure-water-pipette-equipment.jpg" alt="" /></figure>
   <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 className="card-title">Engineering Toys</h2>
+    <h2 className="text-lg">price:$19.99</h2>
+    <p>Rating: 4.2</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <button className="btn btn-primary">View Details</button>
     </div>
   </div>
 </div>
-    <div className="card card-compact w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+<div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://i.ibb.co/ByMg6TK/stock-photo-wooden-toy-blocks-school-supplies-math-fractions-pencils-numbers-on-beige-background-bac.jpg" alt="" /></figure>
   <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <h2 className="card-title">Wooden toy blocks</h2>
+    <h2 className="text-lg">price:$19.99</h2>
+    <p>Rating: 4.3</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+      <button className="btn btn-primary">View Details</button>
+    </div>
+  </div>
+</div>
+    </div>
+    </TabPanel>
+    <TabPanel>
+    <div className="flex justify-center gap-10 m-10">
+    <div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://i.ibb.co/VL56N7B/stock-photo-portrait-of-child-in-classroom-kid-with-toy-virtual-reality-headset-in-class-success-ide.jpg" alt="" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">Kid with toy virtual reality headset in class</h2>
+    <h2 className="text-lg">price:$19.99</h2>
+    <p>Rating: 4.5</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">View Details</button>
+    </div>
+  </div>
+</div>
+<div className="card card-compact w-96 bg-base-100 shadow-xl">
+  <figure><img src="https://i.ibb.co/gF3zdkQ/stock-photo-a-small-nerd-in-glasses-is-holding-a-robot-a-boy-in-a-gray-shirt-and-glasses-is-sitting.jpg" alt="" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">A small nerd in glasses is holding a robot</h2>
+    <h2 className="text-lg">price:$19.99</h2>
+    <p>Rating: 4.3</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">View Details</button>
     </div>
   </div>
 </div>

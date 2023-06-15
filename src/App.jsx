d9dfa2fe50d365408e64185183bf1c7c4e61d598
Md.Mohiuddin <saddammd203@gmail.com>
2023-06-15 10:37:29 +0600

    add new navabar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
+import Navbar1 from './components/Navbar/Navbar1'
 
 
 
@@ -12,6 +13,7 @@ function App() {
   return (
     <div className='min-h-screen'>
       <Navbar></Navbar>
+      <Navbar1></Navbar1>
  <Outlet></Outlet>
 <Footer></Footer>
     </div>
